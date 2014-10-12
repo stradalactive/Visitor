8aef85db10037d2dee21734f4ecd35217eb0f35f
esstradal <eric@ericstradal.com>
2014-10-12 10:23:30 -0600

    Initial Commit

---
+++ Visitor/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Visitor
+//
+//  Created by Eric Stradal on 10/12/14.
+//  Copyright (c) 2014 Eric Stradal. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
